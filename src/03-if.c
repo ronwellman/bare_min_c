a1e9cffdcd85a293d530ab23028730723efee816
Ron Wellman <ron.wellman01@gmail.com>
2020-07-02 12:26:04 -0400

    If and switch.

---
+++ src/03-if.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int
+main() {
+    unsigned proto = 17;
+
+    if (6 == proto) {
+        printf("TCP traffic detected...\n");
+    }
+    else if (17 == proto) {
+        printf("UDP traffic detected...\n");
+    }
+    else {
+        printf("Unknown protocol number detected...\n");
+    }
+    
+    return 0;
+}
