1ca703683826da9575d7be7b9838cf10d6336f23
ronwellman <ron.wellman01@gmail.com>
2020-08-03 22:58:41 -0400

    Begin chapter 7 on concurrency.

---
+++ src/07-concurrency1.c
@@ -0,0 +1,48 @@
+#define _POSIX_C_SOURCE 199309L
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <time.h>
+
+void
+echo(const char *, const char *);
+
+int
+main() {
+	const char *shared = "I am a shared string!";
+	
+	pid_t myPid;
+	pid_t parentPid;
+	pid_t newPid;
+
+	myPid = getpid();
+	parentPid = getppid();
+	printf("Program begins with:\n\tPID: %d\n\tParentPid: %d\n",
+			myPid, parentPid);
+
+	newPid = fork();
+	if (-1 == newPid) {
+		perror("Unable to fork.");
+	}
+	else if (0 == newPid) {
+		parentPid = getppid();
+		printf("Child Process:\n\tPID: %d\n\tParentPid: %d\n",
+				newPid, parentPid);
+		echo("CHILD", shared);
+	} 
+	else {
+		printf("My Child's PID: %d\n", newPid);
+		echo("PARENT", shared);
+	}
+}
+
+void
+echo(const char *name, const char *saying) {
+	struct timespec tsReq, tsRem;
+	tsReq.tv_sec = 0;
+	tsRem.tv_nsec = 200000000;
+	for (int i = 0; i < 5; i++) {
+		printf("%s: %s\n", name, saying);
+		nanosleep(&tsReq, &tsRem);
+	}
+}
