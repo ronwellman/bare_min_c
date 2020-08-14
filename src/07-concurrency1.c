ef86b3de160ff782120ecd6cf7d53ae2b943db2c
ronwellman <ron.wellman01@gmail.com>
2020-08-13 22:15:57 -0400

    Commenting and formatting 07-concurrency1.c

--- src/07-concurrency1.c
+++ src/07-concurrency1.c
@@ -10,7 +10,7 @@ echo(const char *, const char *);
 int
 main() {
 	const char *shared = "I am a shared string!";
-	
+
 	pid_t myPid;
 	pid_t parentPid;
 	pid_t newPid;
@@ -21,16 +21,20 @@ main() {
 			myPid, parentPid);
 
 	newPid = fork();
+	
 	if (-1 == newPid) {
+		/* error */
 		perror("Unable to fork.");
 	}
 	else if (0 == newPid) {
+		/* child */
 		parentPid = getppid();
 		printf("Child Process:\n\tPID: %d\n\tParentPid: %d\n",
 				newPid, parentPid);
 		echo("CHILD", shared);
 	} 
 	else {
+		/* parent */
 		printf("My Child's PID: %d\n", newPid);
 		echo("PARENT", shared);
 	}
@@ -38,9 +42,11 @@ main() {
 
 void
 echo(const char *name, const char *saying) {
+	/* set timer */
 	struct timespec tsReq, tsRem;
 	tsReq.tv_sec = 0;
-	tsRem.tv_nsec = 200000000;
+	tsReq.tv_nsec = 200000000;
+	
 	for (int i = 0; i < 5; i++) {
 		printf("%s: %s\n", name, saying);
 		nanosleep(&tsReq, &tsRem);
